92c139bf0fb8aeaec9de2ef0abb3aae3dabbb899
kkuop <60794860+kkuop@users.noreply.github.com>
2020-03-17 16:51:15 -0500

    created DbSet properties for Customer and Employee in the ApplicationDbContext class

--- TrashCollectorWebApp/Data/ApplicationDbContext.cs
+++ TrashCollectorWebApp/Data/ApplicationDbContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TrashCollectorWebApp.Models;
 
 namespace TrashCollectorWebApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Customer> Customer { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
